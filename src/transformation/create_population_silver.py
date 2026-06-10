67095923d8122a9cff45e0a673705df40a049c64
Rodrigo Monteiro <rodrigo.monteiro@foursys.com.br>
2026-06-10 13:45:23 -0300

    "feat: create silver layer with normalized population dataset"

--- src/transformation/create_population_silver.py
+++ src/transformation/create_population_silver.py
@@ -3,14 +3,35 @@ import pandas as pd
 #Leitura do arquivo Bronze
 INPUT_FILE = "data/bronze/population_total.csv"
 
-df = pd.read_csv(INPUT_FILE,skiprows=4)
+df = pd.read_csv(INPUT_FILE, skiprows=4)
 
-print(f"Rows before transformation: {df.shape[0]}")
-print(f"Coluns before transformation:{df.shape[1]}")
+print(f"Input dataset: {df.shape}")
 
-#excluir coluna "unnamed 70"
 df = df.drop(columns=["Unnamed: 70"])
-print(f"Columns after cleanup:{df.shape[1]}")
 
-#for col in df.columns:
-#    print(f"'{col}'")
+year_columns = df.columns[4:]
+
+silver_df = pd.melt(
+    df,
+    id_vars=["Country Name", "Country Code"],
+    value_vars=year_columns,
+    var_name="Year",
+    value_name="Population"
+)
+
+print(f"Silver dataset: {silver_df.shape}")
+
+silver_df.to_csv(
+    "data/silver/population_by_country_year.csv",
+    index=False
+)
+
+print("Silver layer generated successfully.")
+
+#testes 
+
+loaded_df = pd.read_csv(
+    "data/silver/population_by_country_year.csv"
+)
+
+print(loaded_df.shape)
