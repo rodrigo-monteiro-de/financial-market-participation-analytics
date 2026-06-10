3342db87081c26431b8596224481c1cf4a7f7157
Rodrigo Monteiro <rodrigo.monteiro@foursys.com.br>
2026-06-10 08:46:55 -0300

    feat: dropped trash column [Unnamed: 70] from raw data(bronze)

--- src/transformation/create_population_silver.py
+++ src/transformation/create_population_silver.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
+#Leitura do arquivo Bronze
 INPUT_FILE = "data/bronze/population_total.csv"
 
 df = pd.read_csv(INPUT_FILE,skiprows=4)
 
 print(f"Rows before transformation: {df.shape[0]}")
 print(f"Coluns before transformation:{df.shape[1]}")
+
+#excluir coluna "unnamed 70"
+df = df.drop(columns=["Unnamed: 70"])
+print(f"Columns after cleanup:{df.shape[1]}")
+
+#for col in df.columns:
+#    print(f"'{col}'")
